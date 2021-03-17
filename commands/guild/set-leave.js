35806f304a43de9f758f0e9e51e4feeb8cf13076
Xiliris <76891458+Xiliris@users.noreply.github.com>
2021-03-17 17:49:15 +0100

    Update set-leave.js

--- commands/guild/set-leave.js
+++ commands/guild/set-leave.js
@@ -1,24 +1,55 @@
 const welcomeSchema = require("../../schemas/welcome-schema");
 module.exports = {
   commands: "set-leave-message",
-  expectedArgs:
-    "<Some Welcome Message> \n\n~Player~ for username\n~PlayerTag~ for tag~\n~Guild~ for guild name\n~Count~ for member count\n\nTo work it needs to be typed just like this!",
-  minArgs: 2,
   permissions: ["ADMINISTRATOR"],
   callback: async (message, arguments, Discord, client) => {
-    const text = arguments.slice(0).join(" ");
-    await welcomeSchema.findOneAndUpdate(
-      {
-        guildId: message.guild.id,
-      },
-      {
-        guildId: message.guild.id,
-        leaveMessage: text,
-      },
-      {
-        upsert: true,
-      }
-    );
-    message.channel.send(text);
+    const members = message.guild.members.cache.filter(
+      (member) => !member.user.bot
+    ).size;
+    const filter = (m) => m.content;
+    message.channel
+      .send(
+        "**Enter your message, you can use:\n~Player~ for Username\n~PlayerTag~ for Mention\n~Count~ for MemberCount\n~Guild~ for Server Name**"
+      )
+      .then(async () => {
+        message.channel
+          .awaitMessages(filter, { max: 1, time: 1000 * 60, errors: ["time"] })
+          .then(async (collected) => {
+            await welcomeSchema.findOneAndUpdate(
+              {
+                guildId: message.guild.id,
+              },
+              {
+                guildId: message.guild.id,
+                leaveMessage: collected.first().content,
+              },
+              {
+                upsert: true,
+              }
+            );
+            const text4 = collected
+              .first()
+              .content.replace(/~Player~/g, `${message.member.user.tag}`);
+            const text3 = text4.replace(
+              /~PlayerTag~/g,
+              `<@${message.member.user.id}>`
+            );
+            const text2 = text3.replace(
+              /~Guild~/g,
+              `${message.member.guild.name}`
+            );
+            const text1 = text2.replace(/~Count~/g, `${members}`);
+            const embed = new Discord.MessageEmbed()
+              .setAuthor(
+                message.author.tag,
+                message.author.displayAvatarURL(String)
+              )
+              .addFields({
+                name: "Leave Text",
+                value: text1,
+              });
+            message.channel.send(embed);
+          });
+      });
   },
 };
