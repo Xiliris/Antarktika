e184d4cd984e81a71ebb03b30b503a1bacd68e86
Xiliris <76891458+Xiliris@users.noreply.github.com>
2021-03-17 17:29:26 +0100

    Update reaction-role.js

--- server/reaction-role.js
+++ server/reaction-role.js
@@ -1,43 +1,46 @@
 const reactionSchema = require("../schemas/reaction-schema");
 module.exports = async (client) => {
   client.on("messageReactionAdd", async (reaction, user) => {
-    const results = await reactionSchema.find();
+    const results = await reactionSchema.find({});
     for (result of results) {
       const { guildId, channelId, messageId, roles } = result;
       if (user.bot) return;
-      if (channelId !== reaction.message.channel.id) return;
-      if (messageId !== reaction.message.id) return;
+      if (channelId !== reaction.message.channel.id) {
+      } else if (messageId !== reaction.message.id) {
+      } else {
+        const guild = client.guilds.cache.get(guildId);
 
-      const guild = client.guilds.cache.get(guildId);
-
-      for (reactions of roles) {
-        let { role, emoji } = reactions;
-        if (emoji === reaction._emoji.name) {
-          let roleSet = guild.roles.cache.get(role);
-          if (!roleSet) return;
-          let target = guild.members.cache.get(user.id);
-          target.roles.add(roleSet);
+        for (reactions of roles) {
+          let { role, emoji } = reactions;
+          if (emoji === reaction._emoji.name) {
+            let roleSet = guild.roles.cache.get(role);
+            if (!roleSet) return;
+            let target = guild.members.cache.get(user.id);
+            target.roles.add(roleSet);
+          }
         }
       }
     }
   });
+
   client.on("messageReactionRemove", async (reaction, user) => {
     const results = await reactionSchema.find();
     for (result of results) {
       const { guildId, channelId, messageId, roles } = result;
 
-      if (channelId !== reaction.message.channel.id) return;
-      if (messageId !== reaction.message.id) return;
+      if (channelId !== reaction.message.channel.id) {
+      } else if (messageId !== reaction.message.id) {
+      } else {
+        const guild = client.guilds.cache.get(guildId);
 
-      const guild = client.guilds.cache.get(guildId);
-
-      for (reactions of roles) {
-        let { role, emoji } = reactions;
-        if (emoji === reaction._emoji.name) {
-          let roleSet = guild.roles.cache.get(role);
-          if (!roleSet) return;
-          let target = guild.members.cache.get(user.id);
-          target.roles.remove(roleSet);
+        for (reactions of roles) {
+          let { role, emoji } = reactions;
+          if (emoji === reaction._emoji.name) {
+            let roleSet = guild.roles.cache.get(role);
+            if (!roleSet) return;
+            let target = guild.members.cache.get(user.id);
+            target.roles.remove(roleSet);
+          }
         }
       }
     }
